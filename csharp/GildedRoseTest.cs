ad204c0f36debee6efe3b0aa97bf90aa21129f8e
Jacques Ophoff <j.ophoff@abertay.ac.uk>
2023-11-28 17:21:57 +0000

    Updated unit test

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -1,18 +1,17 @@
-﻿using NUnit.Framework;
+﻿using Xunit;
 using System.Collections.Generic;
 
 namespace csharp
 {
-    [TestFixture]
     public class GildedRoseTest
     {
-        [Test]
-        public void foo()
+        [Fact]
+        public void Foo()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.Equal("fixme", Items[0].Name);
         }
     }
 }
